933485fa871158fadcdd5212642ebe778a7e324e
Adam Piper <adam@ahri.net>
2011-12-12 22:00:32 +0000

    Develop idea to fruition. Alpha level release ready.

--- nestache/nestview.py
+++ nestache/nestview.py
@@ -1,14 +1,61 @@
+import os
+from pystache import Template
+
 class View(object):
 
     """
     """
 
-    template_path = '.'
-    template_extension = 'mustache'
-    template_name = None
-    template_file = None
-    template = None
-    template_encoding = None
+    lax                = False
+    hooks              = {}
 
-    def render(self):
-        raise IOError()
+    template           = None
+    template_file      = None
+    template_encoding  = None
+    template_name      = None
+    template_extension = 'mustache'
+    template_path      = '.'
+
+    def _resolve_vartype(self, var, cls):
+        try:
+            return var.get(cls)
+        except AttributeError:
+            return var
+
+    def _resolve_template(self, cls):
+        return self._resolve_vartype(self.template, cls) or \
+            open(self._resolve_full_path(cls)).read()
+
+    def _resolve_file(self, name, cls):
+        filename = self._resolve_vartype(self.template_file, cls) or \
+            "%(name)s.%(ext)s" % dict(name=name,
+                                      ext=self.template_extension)
+
+        return "%(path)s%(sep)s%(filename)s" % dict(path=self.template_path,
+                                                    sep=os.sep,
+                                                    filename=filename)
+
+    def _resolve_name(self, cls):
+        return self._resolve_vartype(self.template_name, cls) or \
+            self.__class__.__name__
+
+    def _resolve_full_path(self, cls):
+        return self._resolve_file(self._resolve_name(cls), cls)
+
+    def get(self, attr, _):
+        try:
+            return self.render(self.hooks[attr])
+        except KeyError:
+            try:
+                return getattr(self, attr)()
+            except AttributeError:
+                if self.lax:
+                    return ''
+                raise
+
+    def render(self, cls=None):
+        if cls is None:
+            # pick first subclass of View
+            cls = self.__class__.mro()[:-2][-1]
+
+        return Template(self._resolve_template(cls), self).render()
--- tests/test_nesting.py
+++ tests/test_nesting.py
@@ -19,11 +19,25 @@ class Child(Base):
     The child of the base class
     """
 
+    def __init__(self):
+        super(Child, self).__init__()
+        self.hooks['c_tgt'] = Child
+
+    def child_var(self):
+        return "child_var"
+
 class AnotherChild(Child):
 
     """
     An extra child
     """
+
+    def __init__(self):
+        super(AnotherChild, self).__init__()
+        self.hooks['ac_tgt'] = AnotherChild
+
+    def another_child_var(self):
+        return "another_child_var"
 
 class Nesting(TestCase):
 
@@ -33,5 +47,122 @@ class Nesting(TestCase):
 
     def test_tpl_missing(self):
         """No valid template"""
-        # Arrange, Act, Assert
-        self.assertRaises(IOError, Base().render)
+        # Arrange
+        b = Base()
+        b.template_file = 'non-existant'
+
+        # Act, Assert
+        self.assertRaises(IOError, b.render)
+
+    def test_tpl_resolve_no_hints(self):
+        """Provide as few hints as possible for template name resolution"""
+        # Arrange
+        b = Base()
+
+        # Act, Assert
+        self.assertEqual(b._resolve_full_path(Base), "./Base.mustache")
+
+    def test_tpl_resolve_hint_path(self):
+        """Provide the path and resolve the full name"""
+        # Arrange
+        b = Base()
+        b.template_path = "test"
+
+        # Act, Assert
+        self.assertEqual(b._resolve_full_path(Base), "test/Base.mustache")
+
+    def test_tpl_resolve_hint_extension(self):
+        """Provide the extension and resolve the full name"""
+        # Arrange
+        b = Base()
+        b.template_extension = "test"
+
+        # Act, Assert
+        self.assertEqual(b._resolve_full_path(Base), "./Base.test")
+
+    def test_tpl_resolve_hint_name(self):
+        """Provide the name and resolve the full name"""
+        # Arrange
+        b = Base()
+        b.template_name = "test"
+
+        # Act, Assert
+        self.assertEqual(b._resolve_full_path(Base), "./test.mustache")
+
+    def test_tpl_resolve_hint_file(self):
+        """Provide the file and resolve the full name"""
+        # Arrange
+        b = Base()
+        b.template_file = "test"
+
+        # Act, Assert
+        self.assertEqual(b._resolve_full_path(Base), "./test")
+
+    def test_tpl_provide_template(self):
+        """Provide the template"""
+        # Arrange
+        b = Base()
+        b.template = "test"
+
+        # Act, Assert
+        self.assertEqual(b.render(), "test")
+
+    def test_tpl_substitution(self):
+        """Provide the template and a valid substitution"""
+        # Arrange
+        b = Base()
+        b.template = "{{base_var}}"
+
+        # Act, Assert
+        self.assertEqual(b.render(),
+                         render(b.template, dict(base_var=b.base_var())))
+
+    def test_tpl_substitution_missing(self):
+        """Provide the template and an invalid substitution"""
+        # Arrange
+        b = Base()
+        b.template = "{{base_vr}}"
+
+        # Act, Assert
+        self.assertRaises(AttributeError, b.render)
+
+    def test_tpl_substitution_missing_lax(self):
+        """Provide the template and an invalid substitution in lax mode"""
+        # Arrange
+        b = Base()
+        b.template = "{{base_vr}}"
+        b.lax = True
+
+        # Act, Assert
+        self.assertEqual(b.render(), '')
+
+    def test_tpl_levels(self):
+        """Test rendering different levels of a class hierarchy"""
+        # Arrange
+        a = AnotherChild()
+        a.template = { AnotherChild: "{{another_child_var}}",
+                       Child:        "{{child_var}}",
+                       Base:         "{{base_var}}" }
+
+        # Act, Assert
+        self.assertEqual(a.render(Base),
+                         render(a.template.get(Base), dict(base_var=a.base_var())))
+        self.assertEqual(a.render(Child),
+                         render(a.template.get(Child), dict(child_var=a.child_var())))
+        self.assertEqual(a.render(AnotherChild),
+                         render(a.template.get(AnotherChild), dict(another_child_var=a.another_child_var())))
+
+    def test_tpl_nesting(self):
+        """Given a few templates, check that nesting works"""
+        # Arrange
+        a = AnotherChild()
+        a.template = { AnotherChild: "{{another_child_var}}",
+                       Child:        "{{child_var}}{{ac_tgt}}",
+                       Base:         "{{base_var}}{{c_tgt}}" }
+
+        # Act, Assert
+        self.assertFalse(a.lax)
+        self.assertEqual(a.render(),
+                         render(a.template.get(Base), dict(base_var=a.base_var())) + \
+                         render(a.template.get(Child), dict(child_var=a.child_var())) + \
+                         render(a.template.get(AnotherChild), dict(another_child_var=a.another_child_var())))
