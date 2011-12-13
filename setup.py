1042f55f9c46cfc1c0fd3c9ba7f8e1f432184c63
Adam Piper <adam@ahri.net>
2011-12-13 06:57:37 +0000

    Prep for release.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup
 
 setup(
     name='Nestache',
-    version='0.1',
+    version='0.1.0',
     url='http://github.com/ahri/nestache',
     license='AGPLv3',
     author='Adam Piper',
